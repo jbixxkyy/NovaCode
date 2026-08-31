1928c64f8a8c7a687942c038ba9452c93b840801
jbixxkyy <jblixxkyy@gmail.com>
2026-08-31 02:47:55 -0400

    chore: add missing build-node.ts script

---
+++ script/build-node.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env bun
+/**
+ * Build Node.js native binaries
+ * This script compiles native Node modules if needed
+ */
+
+console.log('Building Node.js native modules...');
+
+// TODO: Add your build logic here
+// Example:
+// - Compile Rust modules
+// - Build native C++ extensions
+// - Process binary assets
+
+export {};
